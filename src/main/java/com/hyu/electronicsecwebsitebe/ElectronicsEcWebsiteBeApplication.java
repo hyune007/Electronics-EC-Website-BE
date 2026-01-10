05e4ed1a3d87be5a17c39a2f2ab01a68ba8acb53
Nguyễn Trường Huy <huyhuyhuy201107@gmail.com>
2026-01-10 15:40:10 +0700

    Init Folders BE

---
+++ src/main/java/com/hyu/electronicsecwebsitebe/ElectronicsEcWebsiteBeApplication.java
@@ -0,0 +1,13 @@
+package com.hyu.electronicsecwebsitebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicsEcWebsiteBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronicsEcWebsiteBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hyu/electronicsecwebsitebe/ElectronicsEcWebsiteBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyu.electronicsecwebsitebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicsEcWebsiteBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
