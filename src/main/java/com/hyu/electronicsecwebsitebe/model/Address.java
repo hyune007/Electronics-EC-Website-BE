60f52fdfcf105d345fb98da71b071351ef2aa3a9
Nguyễn Trường Huy <huyhuyhuy201107@gmail.com>
2026-01-14 10:24:31 +0700

    Done Address model

--- src/main/java/com/hyu/electronicsecwebsitebe/model/Address.java
+++ src/main/java/com/hyu/electronicsecwebsitebe/model/Address.java
@@ -1,4 +1,29 @@
 package com.hyu.electronicsecwebsitebe.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "diachi")
 public class Address {
+    @Id
+    @Column(name = "dc_id", length = 8)
+    private String id;
+
+    @Column(name = "dc_city", length = 50)
+    private String city;
+
+    @Column(name = "dc_ward", length = 50)
+    private String ward;
+
+    @Column(name = "dc_detailaddress", length = 50)
+    private String detailAddress;
+
+    @Column(name = "dc_is_default", length = 255)
+    private boolean isDefault = false;
+
+    @ManyToOne
+    @JoinColumn(name = "kh_id")
+    private Customer customer;
 }
