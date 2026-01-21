e2b58d84b8d53609d2e9f7b7cc88fb6782f52e65
Nguyễn Trường Huy <huyhuyhuy201107@gmail.com>
2026-01-21 20:18:01 +0700

    Jan

--- src/main/java/com/hyu/electronicsecwebsitebe/service/impl/ProductServiceImpl.java
+++ src/main/java/com/hyu/electronicsecwebsitebe/service/impl/ProductServiceImpl.java
@@ -15,40 +15,40 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public Page<Product> getProducts(Pageable pageable, String categoryId, String keyword) {
-        if(categoryId==null&&(keyword==null||keyword.trim().isEmpty())){
-            return productRepository.findAll(pageable);
+        if (categoryId == null && (keyword == null || keyword.trim ().isEmpty ())) {
+            return productRepository.findAll (pageable);
         }
-        if(categoryId==null){
-            return productRepository.findByIdContainingIgnoreCaseOrNameContainingIgnoreCase(keyword, keyword, pageable);
+        if (categoryId == null) {
+            return productRepository.findByIdContainingIgnoreCaseOrNameContainingIgnoreCase (keyword, keyword, pageable);
         }
-        if(keyword==null||keyword.trim().isEmpty()){
-            return productRepository.findByCategory_Id(categoryId, pageable);
+        if (keyword == null || keyword.trim ().isEmpty ()) {
+            return productRepository.findByCategoryId (categoryId, pageable);
         }
-        return productRepository.findByCategoryAndKeyword(categoryId, keyword, pageable);
+        return productRepository.findByCategoryAndKeyword (categoryId, keyword, pageable);
     }
 
     @Override
     public Product findById(String id) {
-        return productRepository.findById(id).orElse(null);
+        return productRepository.findById (id).orElse (null);
     }
 
     @Override
     public Product createProduct(Product product) {
-        return productRepository.save(product);
+        return productRepository.save (product);
     }
 
     @Override
     public Product updateProduct(Product product) {
-        return productRepository.save(product);
+        return productRepository.save (product);
     }
 
     @Override
     public void deleteById(String id) {
-        productRepository.deleteById(id);
+        productRepository.deleteById (id);
     }
 
     @Override
     public boolean existsById(String id) {
-        return productRepository.existsById(id);
+        return productRepository.existsById (id);
     }
 }
