cf78bd70d1a426cce1b451b6056defc8f26ec1fa
Nguyễn Trường Huy <huyhuyhuy201107@gmail.com>
2026-01-22 12:36:07 +0700

    ...

--- src/main/java/com/hyu/electronicsecwebsitebe/controller/ProductController.java
+++ src/main/java/com/hyu/electronicsecwebsitebe/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.hyu.electronicsecwebsitebe.controller;
 
 import com.hyu.electronicsecwebsitebe.model.Product;
 import com.hyu.electronicsecwebsitebe.service.ProductService;
+import com.hyu.electronicsecwebsitebe.service.impl.ProductServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/product")
 public class ProductController {
     @Autowired
-    ProductService productService;
+    private ProductServiceImpl productService;
 
     @GetMapping("/page/{p}/category/{category}/keyword/{q}")
     public ResponseEntity<Page<Product>> getProducts(@PathVariable int p, @PathVariable String category, @PathVariable String q) {
