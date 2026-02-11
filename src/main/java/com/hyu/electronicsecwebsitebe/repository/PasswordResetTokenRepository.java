fb5f8d77d7b822a5f08bc915d88b4601670c4320
Nguyễn Trường Huy <huyhuyhuy201107@gmail.com>
2026-02-11 20:12:11 +0700

    login employee, send reset password mail

--- src/main/java/com/hyu/electronicsecwebsitebe/repository/PasswordResetTokenRepository.java
+++ src/main/java/com/hyu/electronicsecwebsitebe/repository/PasswordResetTokenRepository.java
@@ -1,10 +1,16 @@
 package com.hyu.electronicsecwebsitebe.repository;
 
+import com.hyu.electronicsecwebsitebe.model.Customer;
 import com.hyu.electronicsecwebsitebe.model.PasswordResetToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+    Optional<PasswordResetToken> findByToken(String token);
+
+    void deleteByCustomer(Customer customer);
 }
 
