16005c7d163738e39b03fbcab86aba4312fad607
kohacoder <172766095+kohacoder@users.noreply.github.com>
2026-02-12 10:55:02 +0700

    pull thinhlk into khoahnd

--- src/main/java/com/hyu/electronicsecwebsitebe/repository/AddressRepository.java
+++ src/main/java/com/hyu/electronicsecwebsitebe/repository/AddressRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AddressRepository extends JpaRepository<Address, String> {
     boolean existsByCustomerIdAndCityAndWardAndDetailAddress(String customerId, String city, String ward, String detailAddress);
+
 }
