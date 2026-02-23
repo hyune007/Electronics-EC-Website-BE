db8bb804f9df8f34b8f75230e27f00bf205eb33c
Nguyễn Trường Huy <huyhuyhuy201107@gmail.com>
2026-02-23 14:49:05 +0700

    Sepay sample file

--- src/main/java/com/hyu/electronicsecwebsitebe/config/SecurityConfig.java
+++ src/main/java/com/hyu/electronicsecwebsitebe/config/SecurityConfig.java
@@ -34,6 +34,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors (cors -> {
+                })
                 .csrf (AbstractHttpConfigurer::disable)
                 .sessionManagement (session ->
                         session.sessionCreationPolicy (SessionCreationPolicy.STATELESS))
