c799c7c0e0af4af792feed590f5809bd89509970
kohacoder <172766095+kohacoder@users.noreply.github.com>
2026-03-03 11:10:44 +0700

    chuc nang review

--- src/main/java/com/hyu/electronicsecwebsitebe/model/Review.java
+++ src/main/java/com/hyu/electronicsecwebsitebe/model/Review.java
@@ -1,5 +1,6 @@
 package com.hyu.electronicsecwebsitebe.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -8,12 +9,13 @@ import java.util.Date;
 @Data
 @Entity
 @Table(name = "danhgia")
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
 public class Review {
     @Id
     @Column(name = "dg_id")
     private String id;
 
-    @Column(name = "dg_content")
+    @Column(name = "dg_content", length = 100)
     private String content;
 
     @Column(name = "dg_rating")
@@ -25,9 +27,11 @@ public class Review {
 
     @ManyToOne
     @JoinColumn(name = "sp_id")
+    @JsonIgnoreProperties({ "category", "brand" })
     private Product product;
 
     @ManyToOne
     @JoinColumn(name = "kh_id")
+    @JsonIgnoreProperties({ "password", "role" })
     private Customer customer;
 }
