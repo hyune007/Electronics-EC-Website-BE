9e21da386df03fe2937dcb5357c0677ef5b47e0d
KhangThinh0331 <khangthinh0331@gmail.com>
2026-03-08 20:03:03 +0700

    add free shipping logic

--- src/main/java/com/hyu/electronicsecwebsitebe/service/GhnLocationService.java
+++ src/main/java/com/hyu/electronicsecwebsitebe/service/GhnLocationService.java
@@ -3,6 +3,8 @@ package com.hyu.electronicsecwebsitebe.service;
 import com.hyu.electronicsecwebsitebe.model.Address;
 
 public interface GhnLocationService {
+    Integer getProvinceId(String city);
+
     Integer getDistrictId(String city, String district);
 
     String getWardCode(Integer districtId, String ward);
