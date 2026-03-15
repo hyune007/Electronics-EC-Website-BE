76ede8b5a2a593e289795943dbb529def0edde03
KhangThinh0331 <khangthinh0331@gmail.com>
2026-03-15 18:30:57 +0700

    add scheduler for update product promotion to null when that promotion has expired

--- src/main/java/com/hyu/electronicsecwebsitebe/repository/ProductRepository.java
+++ src/main/java/com/hyu/electronicsecwebsitebe/repository/ProductRepository.java
@@ -1,16 +1,19 @@
 package com.hyu.electronicsecwebsitebe.repository;
 
 import com.hyu.electronicsecwebsitebe.model.Product;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.math.BigDecimal;
+import java.time.Instant;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String>, JpaSpecificationExecutor<Product> {
+    @Modifying
+    @Transactional
+    @Query("UPDATE Product p SET p.promotion = null WHERE p.promotion.endDate < :now")
+    void removeExpiredPromotions(@Param("now") Instant now);
 }
